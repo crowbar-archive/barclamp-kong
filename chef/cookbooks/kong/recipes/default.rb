25413d140a1c70d14af02d909c86b17f1b1205a2
Jason Cannavale <jason.cannavale@rackspace.com>
2011-09-22 16:38:05 -0500

    Kong tarball and recipe created.

--- chef/cookbooks/kong/recipes/default.rb
+++ chef/cookbooks/kong/recipes/default.rb
@@ -6,3 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+cookbook_file "/opt/kong.tar.gz" do
+  source "kong.tar.gz"
+  mode "0644"
+end
+
+execute "tar" do
+  installation_dir = "/opt"
+  cwd installation_dir
+  command "tar -xvzf /opt/kong.tar.gz"
+  action :run
+end
