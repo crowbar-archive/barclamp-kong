dfa3e286c8dc6624753602dc2fe9203916c772f7
Jason Cannavale <jason.cannavale@rackspace.com>
2011-09-22 15:25:19 -0500

    Adding kong cookbook

---
+++ chef/cookbooks/kong/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Rackspace Cloud Builders"
+maintainer_email "jason.cannavale@rackspace.com"
+license          "All rights reserved"
+description      "Installs/Configures kong"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
